1f6f1e740c2fe18cbacd5207a82770b615b14068
Eleni Dimkou <77272704+edimkou@users.noreply.github.com>
2023-04-28 13:32:23 +0300

    Update testbench.v

--- fpga_mem/testbench.v
+++ fpga_mem/testbench.v
@@ -1,15 +1,11 @@
 `timescale 1ns / 1ps
 //////////////////////////////////////////////////////////////////////////////////
+// Author: Dimkou Eleni
 // Company: UTH
-// Design Name: 
+// Design Name: FPAdder-fpga_mem
 // Module Name:    testbench
 // Project Name: Floating Point Adder- testbench
 // Target Devices: Zedboard 
-// Tool versions: 
-// Description: 
-//
-// Dependencies: 
-//
 // Revision: 
 // Revision 0.01 - File Created
 // Additional Comments: 
@@ -23,7 +19,6 @@ reg	clk,rst, en;
 wire an0,  a0,  b0,  c0,  d0,  e0,  f0,  g0;
 wire an1,  a1,  b1,  c1,  d1,  e1,  f1,  g1;
 wire [7:0]leds;
-wire [31:0] out;
 
 	initial
 		begin
@@ -33,30 +28,30 @@ wire [31:0] out;
 
 			#(`CYCLE);
 			rst = 1;
-			$display("1SYSTEM\n an0= %b,  a0= %b,  b0= %b,  c0= %b,  d0= %b,  e0= %b,  f0= %b,  g0= %b,\n an1= %b,  a1= %b,  b1= %b,  c1= %b,  d1= %b,  e1= %b,  f1= %b,  g1= %b\nclk = %b, rst= %b\nout =%h\nen = %b  END SYSTEM\n", an0,  a0,  b0,  c0,  d0,  e0,  f0,  g0, an1,  a1,  b1,  c1,  d1,  e1,  f1,  g1, clk, rst, out, en);
+			$display("1SYSTEM\nan0= %b,  a0= %b,  b0= %b,  c0= %b,  d0= %b,  e0= %b,  f0= %b,  g0= %b,\nan1= %b,  a1= %b,  b1= %b,  c1= %b,  d1= %b,  e1= %b,  f1= %b,  g1= %b\nclk = %b, rst= %b\nen = %b  END SYSTEM\n", an0,  a0,  b0,  c0,  d0,  e0,  f0,  g0, an1,  a1,  b1,  c1,  d1,  e1,  f1,  g1, clk, rst, en);
 			#(`CYCLE);
 			rst = 0;
 			en=1;
 
-			#(130*`CYCLE)$display("2SYSTEM\n an0= %b,  a0= %b,  b0= %b,  c0= %b,  d0= %b,  e0= %b,  f0= %b,  g0= %b,\n an1= %b,  a1= %b,  b1= %b,  c1= %b,  d1= %b,  e1= %b,  f1= %b,  g1= %b\nclk = %b, rst= %b\nout =%h\nen = %b  END SYSTEM\n", an0,  a0,  b0,  c0,  d0,  e0,  f0,  g0, an1,  a1,  b1,  c1,  d1,  e1,  f1,  g1, clk, rst, out, en); // i = 0
+			#(130*`CYCLE)$display("2SYSTEM\nan0= %b,  a0= %b,  b0= %b,  c0= %b,  d0= %b,  e0= %b,  f0= %b,  g0= %b,\nan1= %b,  a1= %b,  b1= %b,  c1= %b,  d1= %b,  e1= %b,  f1= %b,  g1= %b\nclk = %b, rst= %b\nen = %b  END SYSTEM\n", an0,  a0,  b0,  c0,  d0,  e0,  f0,  g0, an1,  a1,  b1,  c1,  d1,  e1,  f1,  g1, clk, rst, en); // i = 0
 
 			en = 0;
-			#(10*`CYCLE) $display("3SYSTEM\n an0= %b,  a0= %b,  b0= %b,  c0= %b,  d0= %b,  e0= %b,  f0= %b,  g0= %b,\n an1= %b,  a1= %b,  b1= %b,  c1= %b,  d1= %b,  e1= %b,  f1= %b,  g1= %b\nclk = %b, rst= %b\nout =%h\nen = %b  END SYSTEM\n", an0,  a0,  b0,  c0,  d0,  e0,  f0,  g0, an1,  a1,  b1,  c1,  d1,  e1,  f1,  g1, clk, rst, out, en); // i = 0
+			#(10*`CYCLE) $display("3SYSTEM\nan0= %b,  a0= %b,  b0= %b,  c0= %b,  d0= %b,  e0= %b,  f0= %b,  g0= %b,\nan1= %b,  a1= %b,  b1= %b,  c1= %b,  d1= %b,  e1= %b,  f1= %b,  g1= %b\nclk = %b, rst= %b\nen = %b  END SYSTEM\n", an0,  a0,  b0,  c0,  d0,  e0,  f0,  g0, an1,  a1,  b1,  c1,  d1,  e1,  f1,  g1, clk, rst, en); // i = 0
 
 			en = 1;
-			#(10*`CYCLE) $display("4SYSTEM\n an0= %b,  a0= %b,  b0= %b,  c0= %b,  d0= %b,  e0= %b,  f0= %b,  g0= %b,\n an1= %b,  a1= %b,  b1= %b,  c1= %b,  d1= %b,  e1= %b,  f1= %b,  g1= %b\nclk = %b, rst= %b\nout =%h\nen = %b  END SYSTEM\n", an0,  a0,  b0,  c0,  d0,  e0,  f0,  g0, an1,  a1,  b1,  c1,  d1,  e1,  f1,  g1, clk, rst, out, en); // i = 1
+			#(10*`CYCLE) $display("4SYSTEM\nan0= %b,  a0= %b,  b0= %b,  c0= %b,  d0= %b,  e0= %b,  f0= %b,  g0= %b,\nan1= %b,  a1= %b,  b1= %b,  c1= %b,  d1= %b,  e1= %b,  f1= %b,  g1= %b\nclk = %b, rst= %b\nen = %b  END SYSTEM\n", an0,  a0,  b0,  c0,  d0,  e0,  f0,  g0, an1,  a1,  b1,  c1,  d1,  e1,  f1,  g1, clk, rst, en); // i = 1
 
 			en = 0;
-			#(100*`CYCLE) $display("5SYSTEM\n an0= %b,  a0= %b,  b0= %b,  c0= %b,  d0= %b,  e0= %b,  f0= %b,  g0= %b,\n an1= %b,  a1= %b,  b1= %b,  c1= %b,  d1= %b,  e1= %b,  f1= %b,  g1= %b\nclk = %b, rst= %b\nout =%h\nen = %b  END SYSTEM\n", an0,  a0,  b0,  c0,  d0,  e0,  f0,  g0, an1,  a1,  b1,  c1,  d1,  e1,  f1,  g1, clk, rst, out, en); // i = 1
+			#(100*`CYCLE) $display("5SYSTEM\nan0= %b,  a0= %b,  b0= %b,  c0= %b,  d0= %b,  e0= %b,  f0= %b,  g0= %b,\nan1= %b,  a1= %b,  b1= %b,  c1= %b,  d1= %b,  e1= %b,  f1= %b,  g1= %b\nclk = %b, rst= %b\nen = %b  END SYSTEM\n", an0,  a0,  b0,  c0,  d0,  e0,  f0,  g0, an1,  a1,  b1,  c1,  d1,  e1,  f1,  g1, clk, rst, en); // i = 1
 
 			en = 1;
-			#(10*`CYCLE) $display("6SYSTEM\n an0= %b,  a0= %b,  b0= %b,  c0= %b,  d0= %b,  e0= %b,  f0= %b,  g0= %b,\n an1= %b,  a1= %b,  b1= %b,  c1= %b,  d1= %b,  e1= %b,  f1= %b,  g1= %b\nclk = %b, rst= %b\nout =%h\nen = %b  END SYSTEM\n", an0,  a0,  b0,  c0,  d0,  e0,  f0,  g0, an1,  a1,  b1,  c1,  d1,  e1,  f1,  g1, clk, rst, out, en); // i = 2
+			#(10*`CYCLE) $display("6SYSTEM\nan0= %b,  a0= %b,  b0= %b,  c0= %b,  d0= %b,  e0= %b,  f0= %b,  g0= %b,\nan1= %b,  a1= %b,  b1= %b,  c1= %b,  d1= %b,  e1= %b,  f1= %b,  g1= %b\nclk = %b, rst= %b\nen = %b  END SYSTEM\n", an0,  a0,  b0,  c0,  d0,  e0,  f0,  g0, an1,  a1,  b1,  c1,  d1,  e1,  f1,  g1, clk, rst, en); // i = 2
 
 			en = 1;
-			#(10*`CYCLE) $display("7SYSTEM\n an0= %b,  a0= %b,  b0= %b,  c0= %b,  d0= %b,  e0= %b,  f0= %b,  g0= %b,\n an1= %b,  a1= %b,  b1= %b,  c1= %b,  d1= %b,  e1= %b,  f1= %b,  g1= %b\nclk = %b, rst= %b\nout =%h\nen = %b  END SYSTEM\n", an0,  a0,  b0,  c0,  d0,  e0,  f0,  g0, an1,  a1,  b1,  c1,  d1,  e1,  f1,  g1, clk, rst, out, en); // i = 2
+			#(10*`CYCLE) $display("7SYSTEM\nan0= %b,  a0= %b,  b0= %b,  c0= %b,  d0= %b,  e0= %b,  f0= %b,  g0= %b,\nan1= %b,  a1= %b,  b1= %b,  c1= %b,  d1= %b,  e1= %b,  f1= %b,  g1= %b\nclk = %b, rst= %b\nen = %b  END SYSTEM\n", an0,  a0,  b0,  c0,  d0,  e0,  f0,  g0, an1,  a1,  b1,  c1,  d1,  e1,  f1,  g1, clk, rst, en); // i = 2
 
 			en = 0;
-			#(90*`CYCLE) $display("8SYSTEM\n an0= %b,  a0= %b,  b0= %b,  c0= %b,  d0= %b,  e0= %b,  f0= %b,  g0= %b,\n an1= %b,  a1= %b,  b1= %b,  c1= %b,  d1= %b,  e1= %b,  f1= %b,  g1= %b\nclk = %b, rst= %b\nout =%h\nen = %b  END SYSTEM\n", an0,  a0,  b0,  c0,  d0,  e0,  f0,  g0, an1,  a1,  b1,  c1,  d1,  e1,  f1,  g1, clk, rst, out, en); // i = 2
+			#(90*`CYCLE) $display("8SYSTEM\nan0= %b,  a0= %b,  b0= %b,  c0= %b,  d0= %b,  e0= %b,  f0= %b,  g0= %b,\nan1= %b,  a1= %b,  b1= %b,  c1= %b,  d1= %b,  e1= %b,  f1= %b,  g1= %b\nclk = %b, rst= %b\nen = %b  END SYSTEM\n", an0,  a0,  b0,  c0,  d0,  e0,  f0,  g0, an1,  a1,  b1,  c1,  d1,  e1,  f1,  g1, clk, rst, en); // i = 2
 
 			$finish;
 		end
@@ -71,8 +66,8 @@ wire [31:0] out;
 					   clk,
                        rst, 
                        en,
-                       leds, 
+                       leds,
                        an0,  a0,  b0,  c0,  d0,  e0,  f0,  g0,
-                       an1,  a1,  b1,  c1,  d1,  e1,  f1,  g1, out);
+                       an1,  a1,  b1,  c1,  d1,  e1,  f1,  g1);
 		
 endmodule
